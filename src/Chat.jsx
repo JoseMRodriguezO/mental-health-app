a1d9e40f0b5288775f539602c8e863e8f2de6bdc
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-03-28 01:21:40 -0500

    Add users to chatbox

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,26 +1,33 @@
 import { useState } from "react";
+import "./Chat.css"; // Import the CSS file
 
 export function Chat() {
   const [messages, setMessages] = useState([]);
+  const users = ["Jose", "Mary"]; // Define two fake users
 
   const sendMessage = (event) => {
     event.preventDefault();
     const messageInput = event.target.elements.message;
     const message = messageInput.value;
-    setMessages([...messages, message]);
+    const user = users[Math.floor(Math.random() * users.length)]; // Pick a random user
+    setMessages([...messages, { user, message }]);
     messageInput.value = "";
   };
 
   return (
-    <div>
-      <ul>
+    <div className="chat-box">
+      <ul className="message-list">
         {messages.map((message, index) => (
-          <li key={index}>{message}</li>
+          <li key={index} className="message-item">
+            <strong>{message.user}:</strong> {message.message}
+          </li>
         ))}
       </ul>
-      <form onSubmit={sendMessage}>
-        <input type="text" name="message" />
-        <button type="submit">Send</button>
+      <form onSubmit={sendMessage} className="message-form">
+        <input type="text" name="message" className="message-input" />
+        <button type="submit" className="send-button">
+          Send
+        </button>
       </form>
     </div>
   );
