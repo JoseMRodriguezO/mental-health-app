e5ff07bc98cbedd6b243dfa26b4c7225e734307d
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-03-28 11:51:17 -0500

    Chatbox withsome CSS

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,28 +1,39 @@
 import { useState } from "react";
-import "./Chat.css"; // Import the CSS file
 
 export function Chat() {
   const [messages, setMessages] = useState([]);
-  const users = ["Jose", "Mary"]; // Define two fake users
+  const users = ["Jose", "Mary"];
 
   const sendMessage = (event) => {
     event.preventDefault();
     const messageInput = event.target.elements.message;
     const message = messageInput.value;
-    const user = users[Math.floor(Math.random() * users.length)]; // Pick a random user
+    const user = users[Math.floor(Math.random() * users.length)];
     setMessages([...messages, { user, message }]);
     messageInput.value = "";
   };
 
   return (
     <div className="chat-box">
-      <ul className="message-list">
-        {messages.map((message, index) => (
-          <li key={index} className="message-item">
-            <strong>{message.user}:</strong> {message.message}
-          </li>
+      <div className="container">
+        {users.map((user, index) => (
+          <div
+            key={index}
+            className={`talk-bubble border round tri-right ${index % 2 === 0 ? "left-top" : "btm-left-in"}`}
+          >
+            <div className="talktext"></div>
+            {messages.map(
+              (message, index) =>
+                message.user === user && (
+                  <li key={index} className="message-item">
+                    <strong>{message.user}:</strong> {message.message}
+                  </li>
+                )
+            )}
+          </div>
         ))}
-      </ul>
+      </div>
+
       <form onSubmit={sendMessage} className="message-form">
         <input type="text" name="message" className="message-input" />
         <button type="submit" className="send-button">
